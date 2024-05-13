450c65d3f2f5e5b961c73ea9fb0ab33219683efb
Carol Fagundes <carolinarftk@gmail.com>
2024-05-13 20:56:23 -0300

    tornando o site dinamico com uma api local

---
+++ js/mostrarVideos.js
@@ -0,0 +1,32 @@
+import { conectaApi } from "./conectaApi.js";
+
+const lista = document.querySelector("[data-lista]");
+
+function constroiCard(titulo, descricao, url, imagem) {
+  //criando um elemento li
+  //template strings
+  const video = document.createElement("li");
+  video.className = "videos__item";
+  video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+</iframe>
+<div class="descricao-video">
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
+</div>`;
+
+  return video;
+}
+
+//funcao pra consumir a funcao da api
+async function listaMostraVideos() {
+  const listaApi = await conectaApi.listaVideos();
+  //adicionando os li na UL
+  listaApi.forEach(elemento => lista.appendChild(
+    constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+}
+
+listaMostraVideos();
