5a720fdedb477d248b17ca74d071845117164dbc
Carol Fagundes <carolinarftk@gmail.com>
2024-05-14 22:32:18 -0300

    terminando projeto

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -30,6 +30,10 @@ async function buscarVideo(evento) {
       )
     )
   );
+
+  if (busca.length == 0) {
+    lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com este termo</h2>`
+  }
 }
 
 const botaoPesquisa = document.querySelector("[data-botao-pesquisa]");
